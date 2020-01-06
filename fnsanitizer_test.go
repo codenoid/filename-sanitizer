470da0aac8d65b312bf8090117536cc71f1676d8
Rubi <14269809+codenoid@users.noreply.github.com>
2020-01-06 15:01:03 +0700

    Create fnsanitizer_test.go

---
+++ fnsanitizer_test.go
@@ -0,0 +1,10 @@
+package fnsanitizer
+
+import "testing"
+
+func TestSanitize(t *testing.T) {
+    got := Sanitize("../../../folder/file-name.3gp")
+    if got != "___folder_file-name.3gp" {
+        t.Errorf("Sanitize("../../../folder/file-name.3gp") = %d; want ___folder_file-name.3gp", got)
+    }
+}
