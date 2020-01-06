c28989a7134dbe45959f735dcb69e48389bea69a
codenoid <14269809+codenoid@users.noreply.github.com>
2020-01-06 15:02:59 +0700

    fix test

--- fnsanitizer_test.go
+++ fnsanitizer_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSanitize(t *testing.T) {
     got := Sanitize("../../../folder/file-name.3gp")
     if got != "___folder_file-name.3gp" {
-        t.Errorf("Sanitize("../../../folder/file-name.3gp") = %d; want ___folder_file-name.3gp", got)
+        t.Errorf(`Sanitize("../../../folder/file-name.3gp") = %v; want ___folder_file-name.3gp`, got)
     }
 }
